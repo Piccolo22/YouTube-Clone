f6c26e86f8147e1ac0d26a9dcb0f8549932f7b37
Venkatesh Mundkar <96167282+Piccolo22@users.noreply.github.com>
2022-02-12 16:16:47 +0530

    Add files via upload
    Old youtube clone

---
+++ YouTube/scripts/signedInHome.js
@@ -0,0 +1,60 @@
+function signedSmallHome(){
+    return `<div id="sMenuBar">
+    <div id="sHome">
+        <img class="borderRadius" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYBhvFKR4DhrLlZJDu7Q4rE7RKCYEOmYAwZg&usqp=CAU" alt="">
+        <p>Home</p>
+    </div>
+    <div id="sExplore">
+        <img class="borderRadius" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQPosmyKMCJgFjQivp9ONln2k3c5brUiMeZMxbxs3Kqvow7cRug" alt="">
+        <p>Explore</p>
+    </div>
+    <div id="sSubscripe">
+        <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR1WK560KQ9JfIpQp-m3tQur40o06CiZgLJNJq2SMXrJgAzzimA" alt="">
+        <p>Subscription</p>
+    </div>
+    <div id="sLibrary">
+        <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcT7YABUHe9INzPu2oSf1RAf3dOfMmhWcyw-orsjowyzdpwvMIG2" alt="">
+        <p>Library</p>
+    </div>
+</div>`
+}
+
+function signedBigHome(){
+    return `<div id="bMenuBar">
+    <div id="bHome" class = "a">
+        <img class="borderRadius" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYBhvFKR4DhrLlZJDu7Q4rE7RKCYEOmYAwZg&usqp=CAU" alt="">
+        <p>Home</p>
+    </div>
+    <div id="bExplore" class = "a">
+        <img class="borderRadius" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQPosmyKMCJgFjQivp9ONln2k3c5brUiMeZMxbxs3Kqvow7cRug" alt="">
+        <p>Explore</p>
+    </div>
+    <div id="bSubscripe" class = "a">
+        <img style="height: 22px; width: 22px; margin-left: 30px;" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR1WK560KQ9JfIpQp-m3tQur40o06CiZgLJNJq2SMXrJgAzzimA" alt="">
+        <p>Subscriptions</p>
+    </div>
+    <hr>
+    <div id="bLibrary" class = "a">
+        <img class="borderRadius" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcT7YABUHe9INzPu2oSf1RAf3dOfMmhWcyw-orsjowyzdpwvMIG2" alt="">
+        <p>Library</p>
+    </div>
+    <div id="sHistory" class = "a">
+        <img class="borderRadius" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTRdJvoCzJ9s1wcF7GVW48tmjNMIrmAlsrVcv568KOIF3RWP96q" alt="">
+        <p>History</p>
+    </div>
+    <div class = "a">
+        <img class="borderRadius" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQorsqkUq_Ujy9_sF22T_Fd44sJd_x40ypV0B2KNgZzGnTFG-ms" alt="">
+        <p>Your Videos</p>
+    </div>
+    <div class = "a">
+        <img class="borderRadius" style="height: 20px; width: 20px; margin-left: 30px;" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRBU6FprW6kIlsOV2EFxUOb9iPfVqlO5WfJ1vVyZ88jXxYpGImp" alt="">
+        <p>Watch Later</p>
+    </div>
+    <div class = "a">
+        <img style="height: 20px; width: 20px; margin-left: 30px;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTrOzGwJZndjYEUzuMN0hXFUbfPO_wF2YLaw&usqp=CAU" alt="">
+        <p>Liked Videos</p>
+    </div>
+</div>`;
+}
+
+export {signedSmallHome, signedBigHome};
